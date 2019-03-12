8e5211aed1bdf65427b4636a2c8c74792d1a4ce0
Hu Minghao <hxdhttk@live.cn>
2019-03-13 05:54:19 +0800

    1 Update project. 2 Introduce new command line args to control sleeping interval.

--- WindowActivator/main.c
+++ WindowActivator/main.c
@@ -9,23 +9,37 @@ int main(int argc, char** argv)
 		exit(-1);
 	}
 
-	char* sWindowTitle = argv[1];
+	char* sWindowTitle = NULL;
+	int iSleepInterval = 500;
+
+	if (argc == 2)
+	{
+		sWindowTitle = argv[1];
+	}
+
+	if (argc == 3)
+	{
+		sWindowTitle = argv[1];
+		iSleepInterval = atoi(argv[2]);
+	}
+
 	HWND hGameWindow = FindWindow(NULL, sWindowTitle);
 	if (hGameWindow != NULL)
 	{
 		printf("Found target window: %p\n", hGameWindow);
+
+		printf("Start sending WM_ACTIVE message to the target window");
 		while (TRUE)
 		{
-			printf("Sending WM_ACTIVE message to the target window.\n");
 			LRESULT lRes = SendMessage(hGameWindow, WM_ACTIVATE, WA_CLICKACTIVE, NULL);
-			printf("SendMessage result: %lld\n", lRes);
 			if (lRes != 0)
 			{
 				printf("SendMessage result: %lld\n", lRes);
 				printf("Maybe we encountered an error, exiting.\n");
 				exit(-1);
 			}
-			Sleep(500);
+			
+			Sleep(iSleepInterval);
 		}
 	}
 
