2d6895fc64c513a93599c225e52740b1eaa43fa2
Minghao Hu <minghaohu@outlook.com>
2022-05-18 14:54:45 +0800

    Add new Op.

--- WindowActivator/main.c
+++ WindowActivator/main.c
@@ -1,41 +1,78 @@
 #include <Windows.h>
 #include <stdio.h>
 
+#define OPKIND_ACT 0
+#define OPKIND_TOP 1
+
 int wmain(int argc, wchar_t** argv)
 {
-	if (argc == 1)
+	if (argc == 2)
 	{
 		printf("Window title is not set!\n");
 		exit(-1);
 	}
 
+	wchar_t* sOp = NULL;
 	wchar_t* sWindowTitle = NULL;
 	int iSleepInterval = 500;
 
-	if (argc == 2)
-	{
-		sWindowTitle = argv[1];
-	}
-
 	if (argc == 3)
 	{
-		sWindowTitle = argv[1];
-		iSleepInterval = _wtoi(argv[2]);
+		sOp = argv[1];
+		sWindowTitle = argv[2];
+	}
+
+	if (argc >= 4)
+	{
+		sOp = argv[1];
+		sWindowTitle = argv[2];
+		iSleepInterval = _wtoi(argv[3]);
+	}
+
+	int opKind = OPKIND_ACT;
+	if (sOp != NULL) {
+		if (lstrcmpiW(sOp, L"act") == 0)
+		{
+			printf("Start sending WM_ACTIVE message to the target window\n");
+			opKind = OPKIND_ACT;
+		}
+		else if (lstrcmpiW(sOp, L"top") == 0)
+		{
+			printf("Start bring the target window to top\n");
+			opKind = OPKIND_TOP;
+		}
+		else
+		{
+			wprintf(L"Unknown OpKind: %s\n", sOp);
+			exit(-1);
+		}
+	}
+	else {
+		printf("OpKind is NULL\n");
+		exit(-1);
 	}
 
 	HWND hGameWindow = FindWindowW(NULL, sWindowTitle);
 	if (hGameWindow != NULL)
 	{
 		printf("Found target window: %p\n", hGameWindow);
-
-		printf("Start sending WM_ACTIVE message to the target window");
+		LRESULT lRes = 0;
+		BOOL succ = FALSE;
 		while (TRUE)
 		{
-			LRESULT lRes = SendMessageW(hGameWindow, WM_ACTIVATE, WA_CLICKACTIVE, NULL);
-			if (lRes != 0)
+			if (opKind == OPKIND_ACT)
 			{
-				printf("SendMessage result: %lld\n", lRes);
-				printf("Maybe we encountered an error, exiting.\n");
+				lRes = SendMessageW(hGameWindow, WM_ACTIVATE, WA_CLICKACTIVE, NULL);
+				succ = lRes == 0;
+			}
+			else if (opKind == OPKIND_TOP)
+			{
+				succ = BringWindowToTop(hGameWindow);
+			}
+
+			if (!succ)
+			{
+				printf("Maybe we encountered an error, exiting...\n");
 				exit(-1);
 			}
 
@@ -43,6 +80,6 @@ int wmain(int argc, wchar_t** argv)
 		}
 	}
 
-	printf("Cannot find target window, exiting...");
+	printf("Cannot find target window, exiting...\n");
 	exit(-1);
 }
